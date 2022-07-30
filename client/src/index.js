3d5d8a3ffb7a08c0046c4409a93c3c3339abca83
Gavin Peng <blackbullet54321@gmail.com>
2022-07-29 18:55:28 -1000

    Fix navbar margin

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'))
-root.render(<App/>);
+root.render(
+    <CssBaseline>
+        <App/>
+    </CssBaseline>
+);
